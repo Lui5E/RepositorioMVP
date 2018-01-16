17dae18f907bf18f1daaad7858136ea0064b998c
Lui5E <luigui_rangel@hotmail.com>
2018-01-15 19:41:19 -0600

    Primer etapa;
    Carga y selección automatica de la WebCap,
    Si existe más de un dispositivo para la captura de imagen se mostrara el combobox para la selección del dispositivo, de lo contrario no mostrara los dispositivos

--- BilboMVP/BilboMVP/Form1.Designer.cs
+++ BilboMVP/BilboMVP/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BilboMVP
 {
-    partial class Form1
+    partial class PantallaPrincipal
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -29,11 +29,96 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.cbxDispositivos = new System.Windows.Forms.ComboBox();
+            this.labelTitulo = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.EstadoDispositivo = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TimerIniciado = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbxDispositivos
+            // 
+            this.cbxDispositivos.FormattingEnabled = true;
+            this.cbxDispositivos.Location = new System.Drawing.Point(579, 12);
+            this.cbxDispositivos.Name = "cbxDispositivos";
+            this.cbxDispositivos.Size = new System.Drawing.Size(121, 21);
+            this.cbxDispositivos.TabIndex = 0;
+            this.cbxDispositivos.Visible = false;
+            // 
+            // labelTitulo
+            // 
+            this.labelTitulo.AutoSize = true;
+            this.labelTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitulo.Location = new System.Drawing.Point(281, 182);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Size = new System.Drawing.Size(127, 42);
+            this.labelTitulo.TabIndex = 1;
+            this.labelTitulo.Text = "BILBO";
+            this.labelTitulo.Click += new System.EventHandler(this.labelTitulo_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.EstadoDispositivo});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 411);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(718, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // EstadoDispositivo
+            // 
+            this.EstadoDispositivo.Name = "EstadoDispositivo";
+            this.EstadoDispositivo.Size = new System.Drawing.Size(68, 17);
+            this.EstadoDispositivo.Text = "Cargando...";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(579, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Captura";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TimerIniciado
+            // 
+            this.TimerIniciado.Interval = 500;
+            this.TimerIniciado.Tick += new System.EventHandler(this.TimerIniciado_Tick);
+            // 
+            // PantallaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(718, 433);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.labelTitulo);
+            this.Controls.Add(this.cbxDispositivos);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "PantallaPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pantalla Principal";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PantallaPrincipal_FormClosing);
+            this.Load += new System.EventHandler(this.PantallaPrincipal_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cbxDispositivos;
+        private System.Windows.Forms.Label labelTitulo;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel EstadoDispositivo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer TimerIniciado;
     }
 }
 
--- BilboMVP/BilboMVP/Form1.cs
+++ BilboMVP/BilboMVP/Form1.cs
@@ -7,14 +7,125 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
 
 namespace BilboMVP
 {
-    public partial class Form1 : Form
+    public partial class PantallaPrincipal : Form
     {
-        public Form1()
+        //Variables globales
+        public bool ExisteDispositivo = false;
+        public FilterInfoCollection DispositivoDeVideo;
+        public VideoCaptureDevice FuenteDeVideo = null;
+        public Bitmap Imagen = null;
+        public int NumImagen = 1;
+        public PantallaPrincipal()
         {
             InitializeComponent();
+            BuscarDispositivos();
+        }
+
+        //Función para cargar las camaras existentes en el combobox
+        public void CargarDispositivos(FilterInfoCollection Dispositivos)
+        {
+            for (int i = 0; i < Dispositivos.Count; i++) ;
+
+            cbxDispositivos.Items.Add(Dispositivos[0].Name.ToString());
+            cbxDispositivos.Text = cbxDispositivos.Items[0].ToString();
+            if(Dispositivos.Count > 2)
+            {
+                cbxDispositivos.Visible = true;
+            }
+            else
+            {
+                cbxDispositivos.Visible = false;
+            }
+        }
+
+        //Función para buscar los dispositivos en el equipo y pasarselos a la función "CargarDispositivos"
+        public void BuscarDispositivos()
+        {
+            DispositivoDeVideo = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (DispositivoDeVideo.Count == 0)
+            {
+                ExisteDispositivo = false;
+            }
+
+            else
+            {
+                ExisteDispositivo = true;
+                CargarDispositivos(DispositivoDeVideo);
+
+            }
+        }
+
+        //Función para tomar la captura de la imagen
+        public void Nuevo_Frame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Imagen = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        //Función para iniciar la conexión con la WebCam
+        public void IniciarFuenteDeVideo()
+        {
+            if (ExisteDispositivo)
+            {
+                FuenteDeVideo = new VideoCaptureDevice(DispositivoDeVideo[cbxDispositivos.SelectedIndex].MonikerString);
+                FuenteDeVideo.NewFrame += new NewFrameEventHandler(Nuevo_Frame);
+                FuenteDeVideo.Start(); 
+                EstadoDispositivo.Text = "Iniciando...";
+            }
+            else
+            {
+                EstadoDispositivo.Text = "No se encontro el dispositivo";
+            }
+        }
+
+        //Función para terminar la conexión con la WebCam
+        public void TerminarFuenteDeVideo()
+        {
+            if (!(FuenteDeVideo == null))
+                if (FuenteDeVideo.IsRunning)
+                {
+                    FuenteDeVideo.SignalToStop();
+                    FuenteDeVideo = null;
+                }
+
+        }
+
+        private void labelTitulo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PantallaPrincipal_Load(object sender, EventArgs e)
+        {
+            IniciarFuenteDeVideo();
+            TimerIniciado.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Imagen.Save(Application.StartupPath + "\\capturas\\captura" + NumImagen.ToString() + ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            NumImagen++;
+        }
+
+        private void PantallaPrincipal_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            TerminarFuenteDeVideo();
+        }
+        
+        //Función para checar cuando ya se haya inicializado la camara
+        private void TimerIniciado_Tick(object sender, EventArgs e)
+        {
+            if(FuenteDeVideo.IsRunning)
+            {
+                EstadoDispositivo.Text = "Listo";
+                TimerIniciado.Stop();
+                TimerIniciado.Enabled = false;
+            }
+            
         }
     }
 }
